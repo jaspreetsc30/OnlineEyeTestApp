49e887da628e8f82d452d50bde43ce9cbfe3f73e
mglalwani <himynameismonesh@gmail.com>
2022-04-12 03:11:02 +0800

    changes

--- lib/Services/api.dart
+++ lib/Services/api.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:application/Classes/user.dart';
+import 'package:application/Pages/testScreens/testScreenComponents.dart';
 
 final String postsURL = "http://127.0.0.1:8000/";
 
@@ -47,5 +48,25 @@ Future<User> Register(RegisterUser user) async {
     // If the server did not return a 201 CREATED response,
     // then throw an exception.
     throw Exception('Failed to register or account has already been created');
+  }
+}
+
+Future<testQuestions> fetchNewTest(LoginUser user) async {
+  final response = await http.post(
+    Uri.parse(postsURL + "api/patient/login"),
+    headers: <String, String>{'Content-Type': 'application/json'},
+    body: jsonEncode(<String, String>{
+      'email': user.email,
+      'password': user.password,
+    }),
+  );
+  if (response.statusCode == 200) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to login. Invalid Username/Password');
   }
 }
